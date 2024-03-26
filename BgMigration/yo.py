42a42130b55adc9b16549fd595b17bad28b168cc
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-03-27 08:57:32 +0900

    Feat: Figure out how to use module in python

---
+++ BgMigration/yo.py
@@ -0,0 +1,12 @@
+import module
+import target_table
+
+table = target_table.datatable()
+
+print(table)
+
+module.add(1,2)
+module.greet("John")
+print(module.PI)
+
+# with this snippet from BgMigration/yo.py:
