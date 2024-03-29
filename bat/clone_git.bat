43196fb17d482294dcaa00cd967a526d7728d46d
= <narrchan69@gmail.com>
2024-03-29 21:40:42 +0900

    lfs force prune update

--- bat/clone_git.bat
+++ bat/clone_git.bat
@@ -4,4 +4,5 @@ git config --global lfs.fetchrecentremoterefs false
 
 git config lfs.https://gitlab.cinamon.me/cinev/CINEVStudio.git/info/lfs.locksverify true
 
-git clone -b art/main-s --single-branch https://gitlab.cinamon.me/cinev/CINEVStudio.git
+git clone -b art/main-s1 --single-branch https://gitlab.cinamon.me/cinev/CINEVStudio.git
+
