40ab7099f2b64c612e93e5cb2a958045b9344447
= <narrchan69@gmail.com>
2024-04-01 01:19:49 +0900

    Add write_list_to_csv function to Lib/__lib_stelle__.py

--- Lib/__lib_stelle__.py
+++ Lib/__lib_stelle__.py
@@ -16,7 +16,13 @@ def get_filenames(directory_path):
     filenames = os.listdir(directory_path)
     return filenames
 
-def write_list_to_csv( data : list , csv_file_path : str ):
+def write_list_to_csv( data : list , csv_file_path : str ): 
+    '''
+    Write a list of data to a CSV file, with each element in the list as a row in the CSV file.
+    사용법 :
+        write_list_to_csv( ['a','b','c','d','e'], 'C:/Users/username/Desktop' ) ... 
+        이렇게 하면 C:/Users/username/Desktop/generated_by_stelle.csv 파일이 생성되고, a,b,c,d,e 가 각각 한 줄씩 들어가게 된다.
+    '''
     # Open the CSV file in write mode
     csv_file_path = csv_file_path + '/generated_by_stelle.csv'
     with open(csv_file_path, 'w', newline='') as csvfile:
@@ -48,4 +54,3 @@ def openFolder( folder_path : str ):
 
 unreal.log('Stelle initialised.')
 
-`
