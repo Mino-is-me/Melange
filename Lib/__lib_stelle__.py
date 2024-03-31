922921c8761eead77eb9ec44ca52ac047ea0659c
= <narrchan69@gmail.com>
2024-04-01 02:02:09 +0900

    stelle 정리

--- Lib/__lib_stelle__.py
+++ Lib/__lib_stelle__.py
@@ -1,29 +1,37 @@
 import os
 import csv
 
-__all__ = ['get_assets_in_folder','get_selected_level_actor','substring','get_filenames','write_list_to_csv','openFolder']
+__all__ = [
+    "substring",
+    "get_filenames",
+    "write_list_to_csv",
+    "openFolder",
+]
 
-def substring( _str : str , _from : str , _to : str ):
+
+def substring(_str: str, _from: str, _to: str) -> str:
     _str = r"{}".format(_str)
-    new_str = _str.replace(_from,_to,-1)
+    new_str = _str.replace(_from, _to, -1)
     print(new_str)
     return new_str
 
-def get_filenames(directory_path):
+
+def get_filenames(directory_path) -> list[str]:
     # Get a list of file names in the directory
     filenames = os.listdir(directory_path)
     return filenames
 
-def write_list_to_csv( data : list , csv_file_path : str ): 
-    '''
+
+def write_list_to_csv(data: list, csv_file_path: str) -> csv:
+    """
     Write a list of data to a CSV file, with each element in the list as a row in the CSV file.
     사용법 :
-        write_list_to_csv( ['a','b','c','d','e'], 'C:/Users/username/Desktop' ) ... 
+        write_list_to_csv( ['a','b','c','d','e'], 'C:/Users/username/Desktop' ) ...
         이렇게 하면 C:/Users/username/Desktop/generated_by_stelle.csv 파일이 생성되고, a,b,c,d,e 가 각각 한 줄씩 들어가게 된다.
-    '''
+    """
     # Open the CSV file in write mode
-    csv_file_path = csv_file_path + '/generated_by_stelle.csv'
-    with open(csv_file_path, 'w', newline='') as csvfile:
+    csv_file_path = csv_file_path + "/generated_by_stelle.csv"
+    with open(csv_file_path, "w", newline="") as csvfile:
         # Create a CSV writer
         writer = csv.writer(csvfile)
         for each in data:
@@ -32,12 +40,14 @@ def write_list_to_csv( data : list , csv_file_path : str ):
 
     return True
 
-def get_this_abs_directory() -> str : #현재 파일 경로 반환 
-    return (os.path.dirname(os.path.realpath(__file__)))
+
+def get_this_abs_directory() -> str:  # 현재 파일 경로 반환
+    return os.path.dirname(os.path.realpath(__file__))
 
 
-def openFolder( folder_path : str ):
+def openFolder(folder_path: str):
     os.startfile(folder_path)
     return True
 
-print('Stelle initialised.')
+
+print("Stelle initialised.")
