41a316f1ea43c305534d1d841085b3ee88373cd8
deemo <deemo@cinamon.io>
2024-04-04 08:41:54 +0900

    Delete unused files

--- BgMigration/module.py
+++
@@ -1,7 +0,0 @@
-def greet(name):
-    print(f"Hello, {name}!")
-
-def add(a, b):
-    return a + b
-
-PI = 3.14159
--- BgMigration/move_sm_in_bp.py
+++ BgMigration/move_sm_in_bp.py
@@ -1,5 +1,9 @@
 import unreal 
 from Lib import __lib_topaz__ as topaz
+
 import importlib
 
 importlib.reload(topaz)
+
+print('hi')
+print(unreal)
