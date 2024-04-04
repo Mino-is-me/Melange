35cc6d0d6dbfb4dc6ff723cc47538cd6f9a18cba
deemo <deemo@cinamon.io>
2024-04-04 09:49:16 +0900

    Gather SM in BP in progress

--- BgMigration/move_sm_in_bp.py
+++ BgMigration/move_sm_in_bp.py
@@ -5,5 +5,23 @@ import importlib
 
 importlib.reload(topaz)
 
-print('hi')
-print(unreal)
+selectedAssets = unreal.EditorUtilityLibrary.get_selected_assets()
+# print(selectedAssets[0])
+staticMeshsInBP = topaz.get_component_by_class(selectedAssets[0], unreal.StaticMeshComponent)
+
+directoryPath = selectedAssets[0].get_path_name()
+part = directoryPath.split('/')
+staticMeshesDirectory = '/'.join(part[:6]) + '/06StaticMeshes' 
+print(staticMeshesDirectory)  
+
+for staticMesh in staticMeshsInBP:
+    # print(staticMesh.static_mesh.get_name())
+    arr = staticMesh.static_mesh.get_path_name().split('/')
+    length = len(arr)
+    staticMeshFilesName = arr[length-1]
+    targetPath = staticMeshesDirectory + '/' + staticMeshFilesName
+    print(targetPath)
+    sourcePath = staticMesh.static_mesh.get_path_name()
+    unreal.EditorAssetLibrary.RenameAsset(sourcePath, targetPath)
+
+    # unreal.EditorAssetLibrary.RenameAsset(staticMesh.static_mesh, targetPath)
