a82fcc7ce28082c8a1360986fb5912600f6638b1
deemo <deemo@cinamon.io>
2024-04-05 10:43:30 +0900

    Update read only bat file

--- bat/uncheck_read_only.bat
+++ bat/uncheck_read_only.bat
@@ -1,3 +1,14 @@
 @echo off
-cd /d %1
-attrib -r *.* /s
+setlocal enabledelayedexpansion
+
+echo Unchecking read-only attribute for files and folders...
+
+cd /d %~dp0
+
+for /r %%i in (*) do (
+    attrib -r "%%i"
+    echo Unchecked read-only attribute for: %%i
+)
+
+echo Done.
+pause
