7186c40dc6613c5399762dbc6edf82570660be82
MINO <38447110+Mino-is-me@users.noreply.github.com>
2024-04-08 13:12:00 +0900

    Dev/lib (#23)
    * Unused asset function edit;
    
    * bat 파일 추가
    
    * git bat add
    
    * Merge branch 'dev/lib' of https://github.com/Mino-is-me/Melange into dev/lib
    
    * 깃이그노어에 pyc 추가, track되고있는 pyc 삭제
    
    * new module kafka for sourcecontrol
    
    * Merge branch 'dev/lib' of https://github.com/Mino-is-me/Melange into dev/lib
    
    * lfs unlock macro add
    
    * Kafka module update
    
    * 작동부 까먹고 안올림 데헷페로~

---
+++ SourceControl/safe_unlock.py
@@ -0,0 +1,16 @@
+from Lib import __lib_topaz__ as topaz
+from Lib import __lib_kafka__ as kafka
+import importlib
+
+importlib.reload(topaz)
+importlib.reload(kafka)
+
+selected = topaz.get_selected_assets()
+
+
+for asset in selected : 
+    asset_path = kafka.get_selected_asset_source_path(asset)
+    asset_path = kafka.remap_uepath_to_filepath(asset_path)
+    kafka.unlock_asset(asset_path)
+    print('Unlocking ' + asset_path)
+    pass
