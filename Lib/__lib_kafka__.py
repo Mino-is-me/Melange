b42d91175188294b84f5841b219879f0f468070f
MINO <38447110+Mino-is-me@users.noreply.github.com>
2024-04-08 13:47:11 +0900

    Fix source control issues (#25)
    * Unused asset function edit;
    
    * bat 파일 추가
    
    * git bat add
    
    * Merge branch 'dev/lib' of https://github.com/Mino-is-me/Melange into dev/lib
    
    * 깃이그노어에 pyc 추가, track되고있는 pyc 삭제
    
    * new module kafka for sourcecontrol
    
    * Merge branch 'dev/lib' of https://github.com/Mino-is-me/Melange into dev/lib
    
    * lfs unlock macro add
    
    * Kafka module update
    
    * 작동부 까먹고 안올림 데헷페로~
    
    * kafka 동작부 수정
    
    * Merge branch 'dev/lib' of https://github.com/Mino-is-me/Melange into dev/lib
    
    * Merge branch 'main' into dev/lib

--- Lib/__lib_kafka__.py
+++ Lib/__lib_kafka__.py
@@ -3,10 +3,12 @@
 import unreal
 import importlib
 import subprocess
+import subprocess
 from Lib import __lib_topaz__ as topaz 
 
 importlib.reload(topaz)
 
+def __init__() -> None:
 def __init__() -> None:
     '''
     # Description: Source Control Support 
@@ -22,7 +24,7 @@ def get_git_path() -> str:
     '''
     git_path = unreal.Paths.project_content_dir()
     git_path = git_path.rsplit('/', 3)[0] + '/'
-    print(git_path)
+    print('Git Path = ' + git_path)
     return git_path
 
 def remap_uepath_to_filepath(uepath: str) -> str: #언리얼 패스 -> 파일 패스로 변환
@@ -32,13 +34,17 @@ def remap_uepath_to_filepath(uepath: str) -> str: #언리얼 패스 -> 파일 �
     projectPath = unreal.Paths.project_dir()
     #print(projectPath)
     filepath = uepath.replace('/Game/', projectPath + 'Content/')
+    #print(projectPath)
+    filepath = uepath.replace('/Game/', projectPath + 'Content/')
     name = filepath.rsplit('.', 1)[0]
+    name = name + '.uasset'
+    print(name)
     name = name + '.uasset'
     print(name)
     return name
 
     
-def get_selected_asset_source_path(asset:unreal.object) -> str:
+def get_selected_asset_source_path(asset:object) -> str:
     '''
     ## Description: Get the source path of the selected asset
     '''
@@ -50,15 +56,18 @@ def lock_asset(asset:str) -> None:
     '''
     ## Description: Lock the asset
     '''
-    command = 'git lfs lock ' + asset
     dir = get_git_path()
+    asset = asset.replace(dir, '')
+    command = 'git lfs lock ' + asset
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, cwd=dir)
     output, error = process.communicate()
     print(output)
 
 def unlock_asset(asset:str) -> None:
-    command = 'git lfs unlock ' + asset
     dir = get_git_path()
+    asset = asset.replace(dir, '')
+    print(asset)
+    command = 'git lfs unlock ' + asset
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, cwd=dir)
     output, error = process.communicate()
     print(output)
