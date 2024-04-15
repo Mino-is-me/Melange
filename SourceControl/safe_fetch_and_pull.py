3ea6fd1078677eccc083e2e741e6e6391de25995
MINO <38447110+Mino-is-me@users.noreply.github.com>
2024-04-15 19:02:45 +0900

    Add scripts for moving and destroying IK BP (#34)
    * urgent -> fetch and pull display dialog box
    
    * pull with rebase -> pull
    
    * Readme 왜 롤백됨?
    
    * feat: add script for moving ik bp
    
    * Merge pull request #31 from Mino-is-me/feature/find-ik-bp
    
    Add script for moving ik bp
    
    * Add SpawnRefball.py script for spawning a refball actor based on the viewport camera's position and rotation (#32)
    
    Co-authored-by: ssony <ssony@cinamon.io>
    
    * Add SpawnRefball.py and DestroyRefball.py scripts (#33)
    
    * Add SpawnRefball.py script for spawning a refball actor based on the viewport camera's position and rotation
    
    * Add DestroyRefball.py script for destroying all instances of BP_RefBall actor in the level

--- SourceControl/safe_fetch_and_pull.py
+++ SourceControl/safe_fetch_and_pull.py
@@ -10,6 +10,8 @@ kafka.execute_console_command('git fetch')
 
 kafka.execute_console_command('git stash')
 
-kafka.execute_console_command('git pull --rebase')
+kafka.execute_console_command('git pull')
 
 kafka.execute_console_command('git stash pop')
+
+kafka.dialog_box('Task Complete', 'Git Fetch & Pull Complete, Assets Appeared in 5~10 Seconds.')
