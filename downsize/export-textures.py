7706b941e2d40688337b5f8a385db4126df01b28
Younsoo Tom <tomandlim@gmail.com>
2024-04-26 17:07:31 +0900

    downsizing script in progress
    * feat: script prototype compeleted
    
    * feat: update scripts
    
    * chore: delete unsed file
    
    ---------
    
    Co-authored-by: deemo <deemo@cinamon.io>

--- downsize/export-textures.py
+++ downsize/export-textures.py
@@ -3,6 +3,7 @@ from Lib import __lib_kafka__ as kafka
 from Lib import __lib_archeron__ as archeron
 from Lib import __lib_stelle__ as stelle
 import unreal, importlib
+import time
 
 importlib.reload(topaz)
 importlib.reload(kafka)
@@ -28,51 +29,61 @@ def export_texture ( texture_asset : unreal.Texture2D, target_file_path : str, e
     exportTask = unreal.AssetExportTask()
     
     exportTask.automated = True
-    
     exportTask.filename = target_file_path
     exportTask.object = texture_asset
-    exportTask.options = exporter
     exportTask.prompt = False
-    tgaExporter = exporter
-    exportTask.exporter = tgaExporter
-    tgaExporter.run_asset_export_task(exportTask)
+    exportTask.exporter = exporter
+
+    exporter.run_asset_export_task(exportTask)
     return True
 
+
+#텍스처 선택
 selectedAssets = unreal.EditorUtilityLibrary.get_selected_assets()
 ImagePathList = []
 
-#선택된 이미지 익스포트
+desired_size = 2048
+
+#선택된 텍스처 익스포트
 for asset in selectedAssets:
     tex_size_x = asset.blueprint_get_size_x()
-    if tex_size_x > 1024 :
+    is_virtual_texture = asset.get_editor_property('virtual_texture_streaming')
+    
+    # 조건
+    needs_export = tex_size_x > desired_size and not is_virtual_texture
+
+    print('need_export : ', needs_export)
+    
+    if needs_export :
+        # 변경 목록 전체숫자 체크 용도
+        ImagePathList.append(asset)
+
         tex_path = asset.get_path_name()
         import_info = asset.get_editor_property('asset_import_data')
         source_file = import_info.get_first_filename()
         
+        #이미지 저장할 드라이브 경로
+        target_drive = 'D:/'
+        source_drive = unreal.Paths.project_dir().split('/')[0] + '/'
+
+        new_tex_path = remap_uepath_to_filepath(tex_path).replace(source_drive, target_drive)
+        file_path: str
         hasPNG = source_file.lower().find('.png')
         hasTGA = source_file.lower().find('.tga')
-        new_tex_path = remap_uepath_to_filepath(tex_path)
 
         if hasPNG != -1:
-            png_tex_path = new_tex_path.replace('.uasset','.png')
             print('This is PNG')
-            export_texture(asset, png_tex_path, unreal.TextureExporterPNG())
-            print(png_tex_path)
-            ImagePathList.append(png_tex_path)
+            file_path = new_tex_path.replace('.uasset','.png')
+            exporter = unreal.TextureExporterPNG()
         elif hasTGA != -1:
             print('This is TGA')
-            tga_tex_path = new_tex_path.replace('.uasset','.tga')
-            export_texture(asset, tga_tex_path, unreal.TextureExporterTGA())
-            print(tga_tex_path)
-            ImagePathList.append(tga_tex_path)
+            exporter = unreal.TextureExporterTGA()
+            file_path = new_tex_path.replace('.uasset','.tga')
         else:
-            print('This is not PNG or TGA Export to PNG') 
-            png_tex_path = new_tex_path.replace('.uasset','.png')
-            export_texture(asset, png_tex_path, unreal.TextureExporterPNG())
-            ImagePathList.append(png_tex_path)
-        
-        # export_texture_to_png(asset,png_tex_path)
-
-#이미지 사이즈 조정
-for imagePath in ImagePathList:
-    stelle.resize_image_file(imagePath, 1024, 1024, True)
+            # to-do > rgba채널 사용하는 텍스처면 tga로 아니면 png로 익스포트하게하기
+            print('This is PNG')
+            exporter = unreal.TextureExporterPNG()
+            file_path = new_tex_path.replace('.uasset','.png')
+        export_texture(asset, file_path, exporter)
+        time.sleep(5)
+print(' ************** ', len(ImagePathList),' ************** ')
