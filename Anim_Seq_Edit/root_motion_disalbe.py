8758a3a830858e43bf7da36e17a6a448c773d641
narr <narr@cinamon.io>
2024-05-09 18:01:23 +0900

    feat: Disable root motion for selected AnimSequence assets
    This code change adds functionality to disable root motion for selected AnimSequence assets. It iterates through the selected assets and checks if root motion is enabled. If so, it disables root motion for the asset. The names of the assets for which root motion was disabled are displayed in the dialog box.

---
+++ Anim_Seq_Edit/root_motion_disalbe.py
@@ -0,0 +1,16 @@
+from Lib import __lib_topaz__ as topaz
+from Lib import __lib_kafka__ as kafka
+import importlib, unreal 
+
+importlib.reload(topaz)
+
+
+selected_assets : unreal.Object = topaz.get_selected_assets()
+asset_names_not_root_motion : str = ''
+
+for asset in selected_assets :
+    if asset.__class__ == unreal.AnimSequence :
+        root_motion_enabled : bool = asset.set_editor_property('enable_root_motion', False)
+        asset_names_not_root_motion = asset_names_not_root_motion + asset.get_name() + '\n'
+        
+kafka.dialog_box('Root Motion Validator', asset_names_not_root_motion + 'are Root Motion Disabled.' )   
