967ae1305719df727c6d3bfb39742fe1b46d6b05
narr <narr@cinamon.io>
2024-05-16 17:41:34 +0900

    feat: Add notify track to animation sequences
    This code change adds a notify track to a list of animation sequences in the `set_notify_track.py` script. The script imports necessary modules, reads a CSV file containing the list of animation paths, and iterates through the desired notify track list. It checks if the notify track already exists for each animation sequence and adds it if not. The script then prints the animation sequence path and the added notify track. Finally, it prints 'Done' when the process is complete.

--- Anim_Seq_Edit/set_notify_track.py
+++ Anim_Seq_Edit/set_notify_track.py
@@ -8,7 +8,9 @@ animList : str = 'D:/Parsing/list.csv'
 
 anims : list[str] = stelle.read_csv_to_list(animList)
 
-desired_notify_track_list : list[str] = ['IK_L', 'IK_R', 'Foot_L', 'Foot_R', 'Hand_L', 'Hand_R', 'Spine', 'Head', 'Root']
+desired_notify_track_list : list[str] = ['IK_L', 'IK_R', 'Foot_L', 'Foot_R', 'Hand_L', 'Hand_R', 'Spine', 'Head', 'Root'] 
+# Notify Track List
+## 빌리한테 리스트 받아서 바꿔야함 
 
 
 anim_sequences : list[unreal.AnimSequence] = []
@@ -19,9 +21,15 @@ for anim in anims :
 
 for anim_sequence in anim_sequences :
     
-    for i in desired_notify_track_list :
-        
+    tracks = unreal.AnimationLibrary.get_animation_notify_track_names(anim_sequence)
+    print('이미 있는 트랙 리스트 : ')
+    print(tracks)
+    
+    need_to_add_notify_track_list = [i for i in desired_notify_track_list if i not in tracks]
+    print('추가해야 할 트랙 리스트 : ')
+    print(need_to_add_notify_track_list)
+    
+    for i in need_to_add_notify_track_list :
         unreal.AnimationLibrary.add_animation_notify_track(anim_sequence, i)
         
-
-print('Done')
+    print(anim_sequence.get_path_name() + '의 Notify Track이 추가되었습니다.   ' + '추가된 Notify Track : ' + ( ', '.join(need_to_add_notify_track_list)))
