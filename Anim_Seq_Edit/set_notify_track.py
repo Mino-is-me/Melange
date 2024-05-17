b809389b4ee834e4d298557a9c550d91170cd17c
narr <narr@cinamon.io>
2024-05-17 15:17:42 +0900

    feat: Add animation sequence path prefix in set_notify_track.py
    This code change adds a prefix to the animation sequence paths in the `set_notify_track.py` script. The prefix is '/Game/Customizing/Motion_Final/'. This change ensures that the correct path is used when adding the notify track to each animation sequence.
    
    Co-authored-by: deemo <deemo@cinamon.io>

--- Anim_Seq_Edit/set_notify_track.py
+++ Anim_Seq_Edit/set_notify_track.py
@@ -8,6 +8,8 @@ animList : str = 'D:/Parsing/list.csv'
 
 anims : list[str] = stelle.read_csv_to_list(animList)
 
+anims = [i in for i in anims '/Game/Customizing/Motion_Final/' + i] ## 경로추가 
+
 desired_notify_track_list : list[str] = ['IK_Pelvis', 'IK_Hand_R', 'Attach_Hand_R', 'IK_Hand_L', 'Attach_Hand_L'] 
 desired_curve_track_list : list[str] = ['IKCurve_Pelvis', 'IKCurve_Hand_R', 'IKCurve_Hand_L']
 # Notify and Curve Track List
